417958a207847eef7f33006960336aba5759038c
laimonata <83599005+laimonata@users.noreply.github.com>
2021-06-30 21:01:30 +0300

    uzduotis su krepseliu

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -33,26 +33,51 @@ var pasisveikinimas = 'Labas lietuva!';
 
 
 
-
-
 document.getElementById('kvietejas').addEventListener("click", ikrepseli);
 
 function ikrepseli() {
 
     var kiekis = document.getElementById('kiekis').value;
-    var zodis  = 'prekes';
 
+    var zodis = 'prekes';
+    
     if(kiekis%10 == 1) {
         zodis  = 'prekę';
     }
-
     if( (kiekis > 10 && kiekis < 20) || kiekis%10 == 0) {
         zodis = 'prekių';
     } 
 
+    if( (kiekis%100 > 10 && kiekis%100 < 20) || kiekis%10 == 0) {
+        zodis = 'prekių';
+    } 
+
+    if(kiekis > 100) {
+        var error = document.getElementById('kiekis');
+        error.classList.add('error');
+        alert("Jūsų pasirinktas prekių kiekis yra didesnis nei leistinas");
+    }
+
+    if(kiekis < 1) {
+        var error = document.getElementById('kiekis');
+        error.classList.add('error');
+        alert("Pasirinktas per mažas kiekis");
+    }
+    
+
     document.getElementById('messages').innerHTML = 'Jūs sėkmingai pridėjote ' + kiekis + ' ' + zodis + '  į savo krepšelį!';
 
     //alert('Jūs sėkmingai pridėjote ' + kiekis + ' prekę į savo krepšelį!');
+}
+
+document.getElementById('reset').addEventListener("click", atnaujinti);
+
+function atnaujinti() {
+
+    document.getElementById("reset").onclick;
+    location.reload();
+
+
 }
 
 //kaunasLietuva(petras);
