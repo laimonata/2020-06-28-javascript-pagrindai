fa39ccb5877b55c00c71806470cbf3f03fc0ade3
laimonata <83599005+laimonata@users.noreply.github.com>
2021-07-01 20:16:55 +0300

    logikos bandymas

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -33,42 +33,38 @@ var pasisveikinimas = 'Labas lietuva!';
 
 
 
+
+
 document.getElementById('kvietejas').addEventListener("click", ikrepseli);
 
 function ikrepseli() {
 
     var kiekis = document.getElementById('kiekis').value;
+    var zodis  = 'prekes';
 
-    var zodis = 'prekes';
-    
     if(kiekis%10 == 1) {
         zodis  = 'prekę';
     }
+
     if( (kiekis > 10 && kiekis < 20) || kiekis%10 == 0) {
         zodis = 'prekių';
     } 
 
-    if( (kiekis%100 > 10 && kiekis%100 < 20) || kiekis%10 == 0) {
-        zodis = 'prekių';
-    } 
+    var zinute = 'Jūs sėkmingai pridėjote ' + kiekis + ' ' + zodis + '  į savo krepšelį!';
 
-    if(kiekis > 100) {
-        var error = document.getElementById('kiekis');
-        error.classList.add('error');
-        alert("Jūsų pasirinktas prekių kiekis yra didesnis nei leistinas");
-    }
+    if(kiekis > 100) 
+         zinute = 'Jūsų pasirinktas prekių kiekis yra didesnis nei leistinas.'
 
-    if(kiekis < 1) {
-        var error = document.getElementById('kiekis');
-        error.classList.add('error');
-        alert("Pasirinktas per mažas kiekis");
-    }
-    
+    if(kiekis < 1)
+         zinute = 'Pasirinktas per mažas kiekis.';
 
-    document.getElementById('messages').innerHTML = 'Jūs sėkmingai pridėjote ' + kiekis + ' ' + zodis + '  į savo krepšelį!';
+    document.getElementById('messages').innerHTML = '';
 
     //alert('Jūs sėkmingai pridėjote ' + kiekis + ' prekę į savo krepšelį!');
 }
+
+//kaunasLietuva(petras);
+
 
 document.getElementById('reset').addEventListener("click", atnaujinti);
 
@@ -76,9 +72,63 @@ function atnaujinti() {
 
     location.reload();
 
+}
+
+var i = 0;
+
+while(i < 10) {
+
+    //console.log(i);
+    i++;
 
 }
 
-//kaunasLietuva(petras);
+i = 0;
+
+do {
+    //console.log('Skaicius yra: ' + i);
+    i++;
+}
+while (i < 10)
 
 
+var petras = [
+    5 , 
+    6, 
+    7 , 
+    8 , 
+    9
+];
+
+//console.log(petras[0]);
+
+const duomenys = [
+    {
+        vardas: "Rokis",
+        veisle: "Vokieciu aviganis",
+        miestas: "Vilnius",
+        amzius: "3"
+    },
+    {
+        vardas: "Milka",
+        veisle: "Foksterjeras",
+        miestas: "Kaunas",
+        amzius: "7"
+    },
+    {
+        vardas: "Amsius",
+        veisle: "Zanenhundas",
+        miestas: "Alytus",
+        amzius: "5"
+    }
+];
+
+for(let eile in duomenys) {
+    console.log(duomenys[eile]['vardas'], duomenys[eile]['veisle'], 
+    duomenys[eile]['miestas'], duomenys[eile]['amzius']);
+    
+}
+
+/*for(let i = 0; i < petras.length; i++) {
+    console.log('Masyvo petras indeksas: ' + petras[i]);
+}*/
